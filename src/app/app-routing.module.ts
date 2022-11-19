343d767e6701465a9a336dfc65ca7172c2fbecec
GonzaBah <go.bahamondes@duocuc.cl>
2022-11-19 19:55:58 -0300

    union de ramas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 const routes: Routes = [
   {
